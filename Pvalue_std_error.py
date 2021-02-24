3f33f1f68cc1d7e6528b6779544b78f1dc4ea8c1
gauravry <gaurav@padipa.net>
2021-02-24 10:28:03 +0300

    Update Pvalue_std_error.py

--- Pvalue_std_error.py
+++ Pvalue_std_error.py
@@ -16,11 +16,12 @@ SE = (mu - req_wt)/(sigma/math.sqrt(n))
 def cdf(x, mu, sigma):
     Z = (x - mu)/sigma
     return 0.5*(1 + math.erf(Z/(math.sqrt(2))))
-
+# We are using the error function to calculate the P-Value
 def cdf2(x, mu, sigma):
     Z = (x - mu)/(sigma/math.sqrt(n))
     return 0.5*(1 + math.erf(Z/(math.sqrt(2))))
 
+# Other Option for CDF
 #cdf2 = lambda x: 0.5*(1 + math.erf((sigma/math.sqrt(n))/(math.sqrt(2))))    
 
 #print(round(cdf(x, mu_sum, sigma_sum), 4))
